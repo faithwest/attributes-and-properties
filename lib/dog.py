ae3612ff178e9d5b01ac5cea872cdf1bf5d1b498
faithwest <faithchepkoech425@gmail.com>
2023-11-27 17:55:27 +0300

    assignment done

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,26 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    APPROVED_BREEDS = [
+            "Mastiff",
+            "Chihuahua",
+            "Corgi",
+            "Shar Pei",
+            "Beagle",
+            "French Bulldog",
+            "Pug",
+            "Pointer"
+        ]
+    def __init__(self, name="Fido", breed="Mastiff"):
+        self.breed = breed
+        self._name = name
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if not isinstance(name, str) or 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print(f'Name must be string between 1 and 25 characters. if empty string.')
+
+        name = property(get_name, set_name)
+
